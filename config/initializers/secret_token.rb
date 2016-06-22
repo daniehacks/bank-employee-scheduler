d758c787f448587d2573e9acf72f2f3761089d25
Danie Banks <dabanks@thoughtworks.com>
2016-06-21 19:21:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreditUnionScheduler::Application.config.secret_key_base = '881dfbcedaa06c3a83acf0afa13e76abd722d1eb80eb60e54653378be980310aefa388cf00b6ffb203840c726570f590d05e98a7851ae42882cf14a392617d7c'
