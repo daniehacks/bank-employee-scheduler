5e78800644cc1263777dd5f9ea473e95e78d1432
Danie Banks <dabanks@thoughtworks.com>
2016-06-21 21:09:40 -0500

    Added Employees to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
 10.times do
 Employee.create(
-	:name =>
+	first_name: Faker::Name.first_name,
+	last_name: Faker::Name.last_name
 	)
+end
 
 
