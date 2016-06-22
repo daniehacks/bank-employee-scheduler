1068e3940afaafd453db5d26718bd0f652afc914
Danie Banks <dabanks@thoughtworks.com>
2016-06-21 21:35:11 -0500

    Changed home to index page and added link to new employees

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ CreditUnionScheduler::Application.routes.draw do
   get "/employees" => 'employees#index'
   get "employees/new" => 'employees#new'
   post "/employees" => 'employees#create'
+  root 'employees#index'
 end
